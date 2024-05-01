724c07508295593895df2e8129e0f87e72550609
seran72 <sdiriba33@gmail.com>
2024-04-30 23:28:45 -0700

    First commit

---
+++ Arrays/containsDuplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> dup = new HashSet<>();
+        for(int i=0; i<nums.length; i++){
+            if (dup.contains(nums[i])){
+                return true;
+            }
+            dup.add(nums[i]);
+        }
+        return false;
+    }
+}
